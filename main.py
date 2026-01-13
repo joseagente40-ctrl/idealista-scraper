cd29efcea43b8f650ffebd58eb2843e1d0f6d9d4
joseagente40-ctrl <joseagente40@gmail.com>
2026-01-13 12:37:29 +0100

    Fix indentation issue in main.py for Gunicorn

--- main.py
+++ main.py
@@ -205,6 +205,4 @@ def get_particulares(city='madrid'):
         }), 500
 
 if __name__ == "__main__":
-    
-
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
